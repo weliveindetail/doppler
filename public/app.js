573606548c32a9a0cf11c6ae030db9f27fd34b6c
stefan <stefan.graenitz@gmail.com>
2018-06-06 18:34:30 +0200

    Submit login data to server

--- public/app.js
+++ public/app.js
@@ -1,29 +1,52 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-  document.getElementById('signin-button').addEventListener('click', function(event) {
-    event.preventDefault()
-    blockstack.redirectToSignIn()
-  })
-  document.getElementById('signout-button').addEventListener('click', function(event) {
-    event.preventDefault()
-    blockstack.signUserOut(window.location.href)
-  })
+function submitPostData(url, params) {
+  var form = $('<form></form>').attr({action: url, method: 'post'});
 
-  function showProfile(profile) {
-    var person = new blockstack.Person(profile)
-    document.getElementById('heading-name').innerHTML = person.name() ? person.name() : "Nameless Person"
-    if(person.avatarUrl()) {
-      document.getElementById('avatar-image').setAttribute('src', person.avatarUrl())
-    }
-    document.getElementById('section-1').style.display = 'none'
-    document.getElementById('section-2').style.display = 'block'
+  for(var key in params) {
+    form.append($('<input></input>').attr({
+      type: 'hidden',
+      name: key,
+      value: params[key]
+    }));
   }
 
+  $('body').append(form);
+  form.submit();
+}
+
+$(function(event) {
   if (blockstack.isUserSignedIn()) {
     var profile = blockstack.loadUserData().profile
-      showProfile(profile)
-  } else if (blockstack.isSignInPending()) {
+
+    if ($('#page-welcome').length > 0) {
+      var person = new blockstack.Person(profile)
+      $('#heading-name').html(person.name() ? person.name() : "Nameless Person")
+      if(person.avatarUrl()) {
+        $('#avatar-image').attr('src', person.avatarUrl())
+      }
+      $('#page-welcome').css({display: 'block'});
+
+      $('#spawn-button').click(function(event) {
+        event.preventDefault();
+        submitPostData('/spawn', {user: 'user@host.tld', password: '1234'});
+      });
+    }
+
+    $('#signout-button').click(function(event) {
+      event.preventDefault();
+      blockstack.signUserOut(window.location.href);
+    });
+  }
+  else if (blockstack.isSignInPending()) {
     blockstack.handlePendingSignIn().then(function(userData) {
       window.location = window.location.origin
     })
+  }
+  else {
+    $('#page-landing').css({display: 'block'});
+
+    $('#signin-button').click(function(event) {
+      event.preventDefault();
+      blockstack.redirectToSignIn();
+    });
   }
 })
--- server.js
+++ server.js
@@ -4,15 +4,10 @@ const opn = require('opn');
 const cheerio = require('cheerio');
 const base64js = require('base64-js');
 const puppeteer = require('puppeteer');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 5000;
-
-// TODO: read from blockstack profile
-const credentials = {
-  user: 'dummy-user-name',
-  password: 'dummy-password'
-};
 
 function allowCrossDomain(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
@@ -22,7 +17,10 @@ function allowCrossDomain(req, res, next) {
 }
 
 app.use(allowCrossDomain);
+//app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use('/', express.static(__dirname + '/public'));
+
 app.listen(port, (err) => {
   if (err) {
     return console.log('something bad happened', err)
@@ -48,9 +46,28 @@ function findElem(html, selectors) {
   return firstMatch;
 }
 
-app.get('/spawn', function (req, res) {
+app.get('/', function (req, res) {
+  res.write(getPageHead());
+
+  // Client selects what's shown
+  res.write(getPageLandingContent());
+  res.write(getPageWelcomeContent());
+
+  res.write(getPageFoot());
+  res.end();
+});
+
+app.post('/spawn', function (req, res) {
+  // TODO: don't dump password!
+  console.log('Signing you in to Google with: ', req.body);
+
+  const credentials = {
+    user: req.body.user,
+    password: req.body.password
+  };
+
   (async () => {
-    const browser = await puppeteer.launch({headless: false}); // 
+    const browser = await puppeteer.launch(); // {headless: false}
     const page = await browser.newPage();
     await page.goto('https://accounts.google.com');
 
@@ -73,6 +90,9 @@ app.get('/spawn', function (req, res) {
                   'alt="' + alt + '" />'
     };
 
+    res.write(getPageHead());
+    res.write("<h1>Signing you in to Google..</h1>");
+
     try {
       // 1st login page
       {
@@ -83,7 +103,7 @@ app.get('/spawn', function (req, res) {
         var input = findElem(html, ['input#Email', 'input[type=text]']);
         if (!input)
           throw 'Failed to detect text input for E-Mail in HTML: \n' + html;
-        await page.type(input, credentials.user, {delay: 100});
+        await page.type(input, credentials.user, {delay: 50});
 
         // Click next or press Enter
         const loginSubmitted = page.waitForNavigation();
@@ -105,7 +125,7 @@ app.get('/spawn', function (req, res) {
         var input = findElem(html, ['input#Passwd', 'input[type=password]']);
         if (!input)
           throw 'Failed to detect text input for Password in HTML: \n' + html;
-        await page.type(input, credentials.password, {delay: 100});
+        await page.type(input, credentials.password, {delay: 50});
 
         // Click sign-in or press Enter
         const passwordSubmitted = page.waitForNavigation();
@@ -124,8 +144,72 @@ app.get('/spawn', function (req, res) {
       res.write('Error: ' + msg);
     }
     finally {
-      await browser.close();
+      var closing = browser.close();
+
+      res.write(getPageFoot());
       res.end();
+
+      await closing;
     }
   })();
-})
+});
+
+function getPageLandingContent() {
+  return `
+    <div class="panel-landing hide" id="page-landing">
+      <h1 class="landing-heading">Hello, Blockstack!</h1>
+      <p class="lead">
+        <a href="#" class="btn btn-primary btn-lg" id="signin-button">
+          Sign In with Blockstack
+        </a>
+      </p>
+    </div>
+  `;
+}
+
+function getPageWelcomeContent() {
+  return `
+    <div class="panel-welcome hide" id="page-welcome">
+      <div class="avatar-section">
+        <img src="https://s3.amazonaws.com/onename/avatar-placeholder.png" class="img-rounded avatar" id="avatar-image">
+      </div>
+      <h1>Dear <span id="heading-name">Anonymous</span>!</h1>
+      <p class="lead">
+        <a href="#" class="btn btn-primary btn-lg" id="spawn-button">
+          Spawn!
+        </a>
+      </p>
+      <p class="lead">
+        <a href="#" class="btn btn-primary btn-lg" id="signout-button">
+          Logout
+        </a>
+      </p>
+    </div>
+  `;
+}
+
+function getPageHead() {
+  return `
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>Hello, Blockstack!</title>
+      <link rel="stylesheet" href="bootstrap.min.css" />
+      <link rel="stylesheet" href="app.css" />
+      <script src="bundle.js"></script>
+      <script src="app.js"></script>
+    </head>
+    <body>
+      <div class="site-wrapper">
+        <div class="site-wrapper-inner" id="page">
+  `;
+}
+
+function getPageFoot() {
+  return `
+        </div>
+      </div>
+    </body>
+    </html>
+  `;
+}
